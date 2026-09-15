4144572b7ac11bfcde9d35c7f4785630578d5546
amgalan <amgalanbatkhuyag@gmail.com>
2026-09-15 18:57:16 -0400

    test multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,14 +3,29 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
 
-        int multiples = 0;
+        System.out.println(multiples(1000, 3, 5));
+    }
 
-        for (int i = 1; i <= 1000; i++) {
+     static int multiples(int n, int a, int b) {
+        int count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int multiples() {
+        int count = 0;
+
+        for(int i = 1; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
-                multiples++;
+                count++;
             }
         }
 
-        System.out.println(multiples);
+        return count;
     }
 }
